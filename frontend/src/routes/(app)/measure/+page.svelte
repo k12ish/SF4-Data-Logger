cdd6ed2ccba23d3b94a1c8a22f5b55a1b24a1ca6
k12ish <k4krish@gmail.com>
2024-05-16 17:57:56 +0100

    added a popup modal!

--- frontend/src/routes/(app)/measure/+page.svelte
+++ frontend/src/routes/(app)/measure/+page.svelte
@@ -1,23 +1,27 @@
 <script>
-	async function onClick(val) {
-		if (!val) {
-			// user clicked "get started"
-			let port = await getArduinoPort();
-			console.log(port);
-			await port.open({ baudRate: 115_200 });
-			const reader = port.readable.getReader();
+	import { Button, Modal } from 'flowbite-svelte';
+	import { onMount } from 'svelte';
+	let defaultModal = false;
+	let data = '';
 
-			// Listen to data coming from the serial device.
-			while (true) {
-				const { value, done } = await reader.read();
-				if (done) {
-					// Allow the serial port to be closed later.
-					reader.releaseLock();
-					break;
-				}
-				// value is a Uint8Array.
-				console.log(value);
+	async function onClick() {
+		defaultModal = false;
+		let port = await getArduinoPort();
+		console.log(port);
+		await port.open({ baudRate: 115_200 });
+		const reader = port.readable.getReader();
+
+		// Listen to data coming from the serial device.
+		while (true) {
+			const { value, done } = await reader.read();
+			if (done) {
+				// Allow the serial port to be closed later.
+				reader.releaseLock();
+				break;
 			}
+			// value is a Uint8Array.
+			console.log(value);
+			data = data + '\n' + value.join();
 		}
 	}
 
@@ -36,4 +40,24 @@
 			return null;
 		}
 	}
+	onMount(() => setTimeout(() => (defaultModal = true), 200));
 </script>
+
+<Modal title="Connect your Arduino!" bind:open={defaultModal} dismissable={false}>
+	<div class="flex w-full justify-center">
+		<img src="/arduino-connect.png" alt="Arduino Connecting over USB to Laptop" />
+	</div>
+	<svelte:fragment slot="footer">
+		<div class="flex w-full justify-center">
+			<Button on:click={() => onClick()}>Done!</Button>
+		</div>
+	</svelte:fragment>
+</Modal>
+
+<div class="relative mx-auto mt-24 max-w-2xl">
+	<div class="rounded-md bg-gray-900 p-4 text-white">
+		<div class="overflow-x-auto">
+			<pre id="code" class="text-gray-300">{data}</pre>
+		</div>
+	</div>
+</div>
