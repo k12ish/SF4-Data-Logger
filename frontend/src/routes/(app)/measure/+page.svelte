b3493254daa9e0d8757effbda1f3d8b0f9bb9f9f
k12ish <k4krish@gmail.com>
2024-05-17 12:51:56 +0100

    REACTIVE PLOT

--- frontend/src/routes/(app)/measure/+page.svelte
+++ frontend/src/routes/(app)/measure/+page.svelte
@@ -1,10 +1,15 @@
-<script>
+<script lang="ts">
 	import { onMount } from 'svelte';
 	import PopupModal from 'components/PopupModal.svelte';
 	let data = '';
 
 	let popupModalComponent;
-	let port;
+	// onMount(() => setTimeout(() => (port = popupModalComponent.launch()), 400));
+
+	import { LayerCake, Svg } from 'layercake';
+	import Line from 'components/EKG/Line.svelte';
+	import AxisX from 'components/EKG/AxisX.svelte';
+	import AxisY from 'components/EKG/AxisY.svelte';
 
 	// async function readPort() {
 	// 	console.log(port);
@@ -23,10 +28,39 @@
 	// 		console.log(value);
 	// 		// data = data + '\n' + value.join();
 	// }
-	
-	onMount(() => setTimeout(() => (port = popupModalComponent.launch()), 400));
 
+	// Define some data
+	let points = [
+		{ x: 0, y: 0 },
+		{ x: 5, y: 10 },
+		{ x: 10, y: 20 },
+		{ x: 15, y: 30 },
+		{ x: 20, y: 40 }
+	];
+
+	let idx = 0;
+	function getRandomInt(min: number, max: number): number {
+		min = Math.ceil(min);
+		max = Math.floor(max);
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+
+	setInterval(() => {
+		const i = getRandomInt(0, points.length - 1);
+		points[i].y = Math.random() * 20;
+		points = points;
+	}, 100);
 </script>
+
+<div class="chart-container">
+	<LayerCake data={points} x="x" y="y">
+		<Svg>
+			<AxisX />
+			<AxisY ticks={4} />
+			<Line />
+		</Svg>
+	</LayerCake>
+</div>
 
 <div class="relative mx-auto mt-24 max-w-2xl">
 	<div class="rounded-md bg-gray-900 p-4 text-white">
@@ -37,3 +71,16 @@
 </div>
 
 <PopupModal bind:this={popupModalComponent} />
+
+<style>
+	/*
+    The wrapper div needs to have an explicit width and height in CSS.
+    It can also be a flexbox child or CSS grid element.
+    The point being it needs dimensions since the <LayerCake> element will
+    expand to fill it.
+  */
+	.chart-container {
+		width: 100%;
+		height: 300px;
+	}
+</style>
