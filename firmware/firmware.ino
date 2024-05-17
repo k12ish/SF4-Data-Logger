d25caf9edbea4e72d6cdaa848d961cb0d1a8b741
jepohle <jep78@cam.ac.uk>
2024-05-17 16:11:40 +0100

    Corrected typo

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -3,6 +3,6 @@ byte message[9] = {129, 163, 102, 111, 111, 163, 98, 97, 114};
 void setup() { Serial.begin(115200); }
 
 void loop() {
-  Serial.write(message);
-  delay(1000)
+  Serial.write(message,9);
+  delay(1000);
 }
