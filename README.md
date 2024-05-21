76417bfb994e5666503f61ccdeea3b3b7e2db204
k12ish <k4krish@gmail.com>
2024-05-21 11:52:26 +0100

    minor refactorings

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SF4-Data-Logger
 Group Code DL1
+
+![workflow status](https://github.com/k12ish/SF4-Data-Logger/actions/workflows/firmware.yml/badge.svg)
--- frontend/src/routes/(content)/+page.svelte
+++ frontend/src/routes/(content)/+page.svelte
@@ -1,5 +1,17 @@
 <script>
-	import Landing from 'components/Landing.svelte';
+	import { Heading, P, Button } from 'flowbite-svelte';
+	import { ArrowRightOutline } from 'flowbite-svelte-icons';
 </script>
 
-<Landing />
+<div class="text-center">
+	<Heading tag="h1" class="mb-4" customSize="text-4xl font-extrabold  md:text-5xl lg:text-6xl">
+		Build your own Electrocardiogram
+	</Heading>
+	<P class="mb-6 text-lg sm:px-16 lg:text-xl xl:px-48 dark:text-gray-400">
+		Measure your heart with an Arduino Uno!
+	</P>
+	<Button href="https://en.wikipedia.org/wiki/Electrocardiography">
+		Learn more
+		<ArrowRightOutline class="ms-2 h-6 w-6" />
+	</Button>
+</div>
