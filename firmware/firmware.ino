feadab283ea2f4f20431aacee94511e4bea44970
jepohle <jep78@cam.ac.uk>
2024-05-28 14:44:04 +0100

    Added change of multiplexer settings to firmware file

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,8 +1,63 @@
-byte message[9] = {129, 163, 102, 111, 111, 163, 98, 97, 114};
+#include <MsgPack.h>
 
-void setup() { Serial.begin(115200); }
+// input to msgpack
+MsgPack::arr_t<int> v {1, 2, 3, 4}; // std::vector or arx::stdx::vector
+int prev;
+int timer;
+String mode;
+
+void setup() {
+    Serial.begin(115200);
+    pinMode(A0, INPUT);
+    pinMode(A1, INPUT);
+    pinMode(A2, INPUT);
+    pinMode(2, OUTPUT);
+    pinMode(3, OUTPUT);
+    pinMode(4, OUTPUT);
+    pinMode(5, OUTPUT);
+    digitalWrite(5, HIGH);
+    // Wait for start message to be sent
+    while (Serial.available()==0){
+
+    }
+    if (Serial.available() != 0){
+      mode = Serial.readString();
+      enable_io(mode);
+    }
+    
+}
 
 void loop() {
-  Serial.write(message,9);
-  delay(1000);
+    prev = micros();
+    v[0] = analogRead(A0);
+    v[1] = analogRead(A1);
+    v[2] = analogRead(A2);
+    v[3] = timer;
+    MsgPack::Packer packer;
+    packer.serialize(v);
+    Serial.write(packer.data(), packer.size());
+    timer = micros()-prev;
+    if (Serial.available() != 0){
+      mode = Serial.readString();
+      enable_io(mode);
+    }
+    delay(2000);
+}
+
+
+void enable_io(String mode){
+  digitalWrite(5, HIGH);
+  if (mode == "Regular"){
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+    digitalWrite(5, LOW);
+  }
+  else if (mode == "Augmented"){
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+    digitalWrite(5, LOW);
+  }
+
 }
