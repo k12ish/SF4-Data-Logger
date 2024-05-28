101c714e4bf86266bcce071c92c00ff8b0ed7987
jepohle <jep78@cam.ac.uk>
2024-05-28 14:44:38 +0100

    Added test of msgpack transmission

--- firmware/tests/msgpack/msgpack.ino
+++ firmware/tests/msgpack/msgpack.ino
@@ -1,28 +1,29 @@
 #include <MsgPack.h>
 
 // input to msgpack
-int i = 123;
-float f = 1.23;
-MsgPack::str_t s = "str"; // std::string or String
-MsgPack::arr_t<int> v {1, 2, 3}; // std::vector or arx::stdx::vector
-MsgPack::map_t<String, float> m {{"one", 1.1}, {"two", 2.2}, {"three", 3.3}}; // std::map or arx::stdx::map
-
-// output from msgpack
-int ri;
-float rf;
-MsgPack::str_t rs;
-MsgPack::arr_t<int> rv;
-MsgPack::map_t<String, float> rm;
+MsgPack::arr_t<int> v {1, 2, 3, 4}; // std::vector or arx::stdx::vector
+int prev;
+int timer;
 
 void setup() {
-    delay(2000);
     Serial.begin(115200);
+    pinMode(A0, INPUT);
+    pinMode(A1, INPUT);
+    pinMode(A2, INPUT);
+   
+    // Wait for start message to be sent
+    while (Serial.available()==0){
+    }
 
-    // serialize to msgpack
+void loop() {
+    prev = micros();
+    v[0] = analogRead(A0);
+    v[1] = analogRead(A1);
+    v[2] = analogRead(A2);
+    v[3] = timer;
     MsgPack::Packer packer;
-    packer.serialize(i, f, s, v, m);
-
+    packer.serialize(v);
     Serial.write(packer.data(), packer.size());
+    timer = micros()-prev;
+    delay(2000);
 }
-
-void loop() {}
