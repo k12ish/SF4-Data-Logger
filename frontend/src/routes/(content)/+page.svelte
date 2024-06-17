90ec3564191464a1ca334ec408cf1f0f55f652e5
k12ish <k4krish@gmail.com>
2024-06-17 22:23:33 +0100

    updated landing page!

--- frontend/src/routes/(content)/+page.svelte
+++ frontend/src/routes/(content)/+page.svelte
@@ -13,14 +13,18 @@
   import { Section, News, HeroHeader, HeroBody } from 'flowbite-svelte-blocks';
   import { Button } from 'flowbite-svelte';
   import { ArrowRightOutline } from 'flowbite-svelte-icons';
+  import { Cta } from 'flowbite-svelte-blocks';
+  import { ArrowRightOutline } from 'flowbite-svelte-icons';
+
+	import ArduinoImage from 'lib/assets/harrison-broadbent-unsplash.jpg';
 </script>
 
 <Section name="heroVisual">
   <div class="mr-auto place-self-center lg:col-span-7">
     <HeroHeader h1Class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white" pClass="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
       <svelte:fragment slot="h1">Measure your Heart</svelte:fragment>
-      <svelte:fragment slot="paragraph">From checkout to global sales tax compliance, companies around the world use Flowbite to simplify their payment stack.</svelte:fragment>
-      <a href="measure/"><Button size="xl" class="inline-flex items-center justify-center mr-3">Get started<ArrowRightOutline size="md" class="ml-2 -mr-1" /></Button> </a>
+      <svelte:fragment slot="paragraph">Your heart, your data, anywhere</svelte:fragment>
+      <a href="measure/"><Button size="xl" class="inline-flex items-center justify-center mr-3">Measure<ArrowRightOutline size="md" class="ml-2 -mr-1" /></Button> </a>
     </HeroHeader>
   </div>
   <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
@@ -35,10 +39,9 @@
 		h2Class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white"
 		pClass="text-gray-500 sm:text-xl dark:text-gray-400"
 	>
-		<svelte:fragment slot="h2">Designed for business teams like yours</svelte:fragment>
+		<svelte:fragment slot="h2">A Market Distruptor</svelte:fragment>
 		<svelte:fragment slot="paragraph"
-			>Here at Flowbite we focus on markets where technology, innovation, and capital can unlock
-			long-term value and drive economic growth.</svelte:fragment
+			>Operates on any device with a web browser, providing a flexible alternative to conventional ECG units</svelte:fragment
 		>
 	</HeroHeader>
 	<FeatureDefault>
@@ -46,61 +49,72 @@
 			<svelte:fragment slot="icon"
 				><ChartPieSolid class="text-primary-600 dark:text-primary-300" /></svelte:fragment
 			>
-			<svelte:fragment slot="h3">Marketing</svelte:fragment>
+			<svelte:fragment slot="h3">Backed by NeuroKit2</svelte:fragment>
 			<svelte:fragment slot="paragraph"
-				>Plan it, create it, launch it. Collaborate seamlessly with all the organization and hit
-				your marketing goals every month with our marketing plan.</svelte:fragment
+				>Utilizes the well-established NeuroKit2 Python library for robust analysis. Plots BPM variation and the mean PQRST cardiac cycle.</svelte:fragment
 			>
 		</FeatureItem>
 		<FeatureItem>
 			<svelte:fragment slot="icon"
 				><LandmarkSolid class="text-primary-600 dark:text-primary-300" /></svelte:fragment
 			>
-			<svelte:fragment slot="h3">Legal</svelte:fragment>
+			<svelte:fragment slot="h3">No Installation Required</svelte:fragment>
 			<svelte:fragment slot="paragraph"
-				>Protect your organization, devices and stay compliant with our structured workflows and
-				custom permissions made for you.</svelte:fragment
+				>Simply plug and play on any machine with a web browser. All data is processed locally, ensuring regulatory compliance and enhanced privacy.</svelte:fragment
 			>
 		</FeatureItem>
 		<FeatureItem>
 			<svelte:fragment slot="icon"
 				><BriefcaseSolid class="text-primary-600 dark:text-primary-300" /></svelte:fragment
 			>
-			<svelte:fragment slot="h3">Business Automation</svelte:fragment>
+			<svelte:fragment slot="h3">Three Measurement Modes</svelte:fragment>
 			<svelte:fragment slot="paragraph"
-				>Auto-assign tasks, send Slack messages, and much more. Now power up with hundreds of new
-				templates to help you get started.</svelte:fragment
+				>Provides enhanced diagnostic capabilities through multiple measurement modes.</svelte:fragment
 			>
 		</FeatureItem>
 		<FeatureItem>
 			<svelte:fragment slot="icon"
 				><DollarOutline class="text-primary-600 dark:text-primary-300" /></svelte:fragment
 			>
-			<svelte:fragment slot="h3">Finance</svelte:fragment>
+			<svelte:fragment slot="h3">Cost-effective</svelte:fragment>
 			<svelte:fragment slot="paragraph"
-				>Audit-proof software built for critical financial operations like month-end close and
-				quarterly budgeting.</svelte:fragment
+				>The device costs less than £15 and our web hosting is free!</svelte:fragment
 			>
 		</FeatureItem>
 		<FeatureItem>
 			<svelte:fragment slot="icon"
 				><RocketSolid class="text-primary-600 dark:text-primary-300" /></svelte:fragment
 			>
-			<svelte:fragment slot="h3">Enterprise Design</svelte:fragment>
+			<svelte:fragment slot="h3">Highly Performant</svelte:fragment>
 			<svelte:fragment slot="paragraph"
-				>Craft beautiful, delightful experiences for both marketing and product with real
-				cross-company collaboration.</svelte:fragment
+				>On average, uses less than half a thread when continuously plotting 5,000 data points at 60FPS.</svelte:fragment
 			>
 		</FeatureItem>
 		<FeatureItem>
 			<svelte:fragment slot="icon"
 				><CogOutline class="text-primary-600 dark:text-primary-300" /></svelte:fragment
 			>
-			<svelte:fragment slot="h3">Operations</svelte:fragment>
+			<svelte:fragment slot="h3">High Sampling Frequency</svelte:fragment>
 			<svelte:fragment slot="paragraph"
-				>Keep your company’s lights on with customizable, iterative, and structured workflows built
-				for all efficient teams and individual.</svelte:fragment
+				>Sampling frequency of 2.3kHz far exceeds the recommended 250 Hz for accurate cardiac analysis.</svelte:fragment
 			>
 		</FeatureItem>
 	</FeatureDefault>
+</Section>
+
+<Section name="ctawithimg">
+  <Cta ctatype="image">
+    <svelte:fragment slot="img">
+		<img src={ArduinoImage} class="w-full" alt="Arduino"/>
+    </svelte:fragment>
+    <svelte:fragment slot="h2">Accessible and Open Hardware</svelte:fragment>
+    <p class="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">
+    Open source design files, with KiCad schematics and layouts for amplifiers, multiplexers, and the entire ECG circuit. 
+    Additionally, simulation results and circuit models are available to help you understand and verify the performance of our device.
+    </p>
+    <a href="https://github.com/k12ish/SF4-Data-Logger" class="inline-flex items-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900">
+      Explore our Repository
+      <ArrowRightOutline size="md" class="ml-2 -mr-1" />
+    </a>
+  </Cta>
 </Section>
